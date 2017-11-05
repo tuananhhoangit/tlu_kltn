8ecfb428ee58ce3bac1aa24c16951f049956eb33
tuananhhoang <montuananh@gmai.com>
2017-11-05 19:14:09 +0700

    Finish Project

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,5 +8,8 @@ class StaticPagesController < ApplicationController
     @sidebar_posts_select = Post.select(:id, :title, :picture).left_outer_joins(:likes)
       .group("posts.id").order("count(likes.id) DESC")
     @sidebar_posts = @sidebar_posts_select.first 5
+    @edit_post_forms = current_user.feed.select(:id, :title, :content, :picture,
+      :user_id, :created_at).order(created_at: :desc).paginate page: params[:page],
+      per_page: Settings.post.post_per_page
   end
 end
